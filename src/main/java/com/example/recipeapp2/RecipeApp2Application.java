8ffa49b56718baec1be14b15a7cf5117be704661
Lubava <Lwm28@rambler.ru>
2023-03-22 22:47:45 +0300

    new commit

---
+++ src/main/java/com/example/recipeapp2/RecipeApp2Application.java
@@ -0,0 +1,13 @@
+package com.example.recipeapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeApp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recipeapp2/RecipeApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipeapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
